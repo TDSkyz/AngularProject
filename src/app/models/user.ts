1204abbffacc023b866a4a23d2152f92fe7b0dde
Thiên Trương <tdthien1997@gmail.com>
2019-12-29 08:35:52 +0700

    Fix model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,13 +4,14 @@ import { NgbDateStruct, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 export class User {
   public user_id: number;
   public username: String;
-  
+  public role: String;
   constructor(
   ){}
 
   public setEmpty(){
     this.user_id = null;
     this.username = null;
+    this.role = null;
   }
 
 }
